b12b9c7f3d28b494cfdd1eb921e89411a8acc6b7
kfrFahim <kfrfahim3@gmail.com>
2023-06-09 16:28:07 +0600

    make admin

--- index.js
+++ index.js
@@ -53,7 +53,18 @@ async function run() {
   })
 
 
+// Get admit
 
+app.patch("/users/admin/:id" , async(req, res)=>{
+  const id = req.params.id;
+  const filter = {_id : new ObjectId(id)};
+  const updateDoc = {
+    $set: {
+      role: "admin"
+    } }
+  const result = await usersCollection.updateOne(filter, updateDoc)  
+  res.send(result)
+})
 
 
 
